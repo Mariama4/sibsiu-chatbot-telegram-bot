527f7fcd68fc30f29775a89bed943888e1851773
mariama4 <mariama4@mail.ru>
2022-10-01 22:20:14 +0700

    upd

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -14,31 +14,33 @@ def messageController(database, data, message, state, bot):
     # Определение типа сообщения
     match data['MESSAGE']['TYPE']:
         case "TEXT":
-            return textMessageController(database, data, message)
+            return textMessageController(data, message, bot)
         case "PHOTO":
-            return photoMessageController(database, data, message, bot)
+            return photoMessageController(data, message, bot)
         case "MEDIA_GROUP":
             return mediaGroupMessageController(data, message, bot)
         case "VIDEO_NOTE":
             return videoNoteMessageController(data, message, bot)
         case "VENUE":
-            return 0
+            return venueMessageController(data, message, bot)
+        case "CONTACT":
+            return contactMessageController(data, message, bot)
         case _:
             # not found
             pass
 
 
-def textMessageController(database, data, message):
+def textMessageController(data, message, bot):
     keyboard = keyboardController(data['BUTTONS'])
     return message.answer(text=data['MESSAGE']['TEXT'],
                           reply_markup=keyboard)
 
 
-def photoMessageController(database, data, message, bot):
+def photoMessageController(data, message, bot):
     keyboard = keyboardController(data['BUTTONS'])
     return bot.send_photo(
         chat_id=message.chat.id,
-        caption=data['MESSAGE']['CAPTION'],
+        caption=data['MESSAGE']['PHOTO_CAPTION'],
         photo=data['MESSAGE']['PHOTO_URL'],
         reply_markup=keyboard
     )
@@ -49,7 +51,7 @@ async def mediaGroupMessageController(data, message, bot):
     mediaGroup = mediaGroupController(data['MESSAGE']['MEDIA'])
     await bot.send_media_group(chat_id=message.chat.id,
                                media=mediaGroup)
-    return await message.answer(text=data['MESSAGE']['CAPTION'],
+    return await message.answer(text=data['MESSAGE']['MEDIA_CAPTION'],
                                 reply_markup=keyboard)
 
 
@@ -58,8 +60,38 @@ async def videoNoteMessageController(data, message, bot):
     await bot.send_video_note(chat_id=message.chat.id,
                               video_note=InputFile.from_url(data['MESSAGE']['VIDEO_NOTE_URL'],
                                                             data['MESSAGE']['VIDEO_NOTE_URL']))
-    return await message.answer(text=data['MESSAGE']['CAPTION'],
+    return await message.answer(text=data['MESSAGE']['VIDEO_NOTE_CAPTION'],
                                 reply_markup=keyboard)
+
+
+async def venueMessageController(data, message, bot):
+    keyboard = keyboardController(data['BUTTONS'])
+    await bot.send_venue(
+        chat_id=message.chat.id,
+        latitude=data['MESSAGE']['VENUE_LATITUDE'],
+        longitude=data['MESSAGE']['VENUE_LONGITUDE'],
+        title=data['MESSAGE']['VENUE_TITLE'],
+        address=data['MESSAGE']['VENUE_ADDRESS'],
+        # reply_markup=keyboard
+    )
+    return await message.answer(
+        text=data['MESSAGE']['VENUE_CAPTION'],
+        reply_markup=keyboard
+    )
+
+
+async def contactMessageController(data, message, bot):
+    keyboard = keyboardController(data['BUTTONS'])
+    await bot.send_contact(
+        chat_id=message.chat.id,
+        phone_number=data['MESSAGE']['CONTACT_PHONE_NUMBER'],
+        first_name=data['MESSAGE']['CONTACT_FIRST_NAME'],
+        last_name=data['MESSAGE']['CONTACT_LAST_NAME']
+    )
+    return await message.answer(
+        text=data['MESSAGE']['CONTACT_CAPTION'],
+        reply_markup=keyboard
+    )
 
 
 def mediaGroupController(media):
